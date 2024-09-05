fdd3067eff8c5492777b79e48b3cbe730f13c300
Calica-Emmanuel <calica.eman@gmail.com>
2024-09-05 12:05:06 +0800

    app.js

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
-// app.js
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const fundsRoutes = require('./routes/funds');
 
-// Middleware
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Routes
+
 app.use('/funds', fundsRoutes);
 
-// Default Route
 app.get('/', (req, res) => {
   res.render('index', { title: 'Barangay Funds Management' });
 });
 
-// Server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
